adfafd1065aa3a4b6f9ce977563541130c90ab0a
이시영 <swy05123@gmail.com>
2024-01-17 15:39:11 +0900

    feat: 스타일 설정

--- todolist/pages/index.tsx
+++ todolist/pages/index.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import { NextPage } from "next";
+import styled from "styled-components";
 
-const index = () => {
-  return <div>hello typescript</div>;
+const Container = styled.div`
+  font-style: italic;
+  padding: 20px;
+`;
+const index: NextPage = () => {
+  return <Container>hello typescript</Container>;
 };
 export default index;
